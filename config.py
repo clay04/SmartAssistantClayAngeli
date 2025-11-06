a44d03fe607fcdea8157f2dc0643cbd5fcc1e16e
clay04 <s22210410@student.unklab.ac.id>
2025-11-06 16:54:36 +0800

    terbaru

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 load_dotenv()
 
 class Config:
-    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY', "AIzaSyAJXtFXt0Yacbvst6OS4JcWQ1Gv4uUXN3E")
+    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY', "AIzaSyDyaXTddxQprpy5GbfflDgoenPNImHENqs")
     GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', "AIzaSyAY9iCrbwGpcw3r40EiNSRT1XL0-O8qxGo")
     LOCATIONIQ_API_KEY = os.getenv('LOCATIONIQ_API_KEY', "pk.d5979551cf32542dd30a8a88acabd113")
     OPENCAGE_API_KEY = os.getenv('OPENCAGE_API_KEY', "19b89e38e01444e493ace9f541f2d3d7")
